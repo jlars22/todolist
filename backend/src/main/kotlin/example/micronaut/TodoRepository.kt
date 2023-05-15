a9aec5d51517087552e578ea39a33a86ae1a2bc5
jlars22 <jlars22@student.sdu.dk>
2023-05-15 19:22:13 +0200

    removed

--- backend/src/main/kotlin/example/micronaut/TodoRepository.kt
+++
@@ -1,9 +0,0 @@
-package example.micronaut
-
-import io.micronaut.data.annotation.Repository
-import io.micronaut.data.repository.CrudRepository
-
-@Repository
-interface TodoRepository : CrudRepository<Todo, Long> {
-    // Define additional methods if needed
-}
