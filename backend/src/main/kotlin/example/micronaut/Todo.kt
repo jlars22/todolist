08bb251c354dbcbc5181d1bc55be6a77dfcc46b8
jlars22 <jlars22@student.sdu.dk>
2023-05-16 07:40:44 +0200

    fixed auto increment issue

--- backend/src/main/kotlin/example/micronaut/Todo.kt
+++ backend/src/main/kotlin/example/micronaut/Todo.kt
@@ -1,19 +1,18 @@
 package example.micronaut
 
-import javax.persistence.Entity
-import javax.persistence.Id
-import javax.persistence.Table
+import javax.persistence.*
 
 @Entity
 @Table(name= "todos")
 data class Todo(
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long,
-    val title: String,
-    val description: String?,
+    val description: String,
     val completed: Boolean
 ) {
-    constructor() : this(0, "", "", false)
+    constructor() : this(0, "", false)
 }
+
 
 
