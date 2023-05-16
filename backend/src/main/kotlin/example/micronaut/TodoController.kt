cc78527e78d0cd0ed27aa4f6fa3ade8786bc6c4d
jlars22 <jlars22@student.sdu.dk>
2023-05-16 19:38:37 +0200

    added listTodos method

--- backend/src/main/kotlin/example/micronaut/TodoController.kt
+++ backend/src/main/kotlin/example/micronaut/TodoController.kt
@@ -3,11 +3,17 @@ package example.micronaut
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
 import io.micronaut.http.annotation.Body
+import io.micronaut.http.annotation.Get
 
 @Controller("/todos")
 class TodoController(private val todoService: TodoService) {
     @Post
     fun createTodo(@Body todo: Todo): Todo {
         return todoService.createTodo(todo)
+    }
+
+    @Get
+    fun listTodos(): Iterable<Todo> {
+        return todoService.listTodos()
     }
 }
--- backend/src/main/kotlin/example/micronaut/TodoService.kt
+++ backend/src/main/kotlin/example/micronaut/TodoService.kt
@@ -7,4 +7,8 @@ class TodoService(private val todoRepository: TodoRepo) {
     fun createTodo(todo: Todo): Todo {
         return todoRepository.save(todo)
     }
+
+    fun listTodos(): Iterable<Todo> {
+        return todoRepository.findAll()
+    }
 }
